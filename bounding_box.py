7f4f779f2ea6d500dae7532f2efe2e5d1f6b7535
Enora Boscher <enoraboscher@gmail.com>
2024-02-16 16:59:32 +0100

    update database

--- bounding_box.py
+++ bounding_box.py
@@ -3,7 +3,6 @@ import argparse
 import supervision as sv
 import numpy as np
 from ultralytics import YOLO
-from pprint import pprint
 
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description = "YOLOv8 live")
@@ -51,7 +50,8 @@ def main():
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
 
-    model = YOLO("yolov8n.pt")
+    # model = YOLO("yolov8n.pt")
+    model = YOLO("runs/detect/train/weights/best.pt")
     box_annotator = sv.BoxAnnotator(
         thickness=2,
         text_thickness=2,
@@ -70,26 +70,31 @@ def main():
         # Visualize the results on the frame
         annotated_frame = result[0].plot()
         detections = result[0].boxes # type Boxes object
+        # print(detections)
 
         tracker = {id: tracker[id] for id in tracker.keys() if id in detections.id.tolist()} # delete the object not in the frame anymore
         
         # pprint(tracker)
 
-        for i in range(len(detections.id)): # go through every element detected by their id (one id per element)
-            classe=detections.cls[i].item() # get the correspondant class
-            x1,y1,x2,y2 = detections.xyxy[i] # get the corr coord
+        if detections.id:
+            print("detections")
+            for i in range(len(detections.id)): # go through every element detected by their id (one id per element)
+                classe=detections.cls[i].item() # get the correspondant class
+                x1,y1,x2,y2 = detections.xyxy[i] # get the corr coord
+                conf_rate = detections.conf[i]
+                print(detections.id)
 
-            if classe == 0: # if is a human           
+                if classe == 0 and conf_rate > 0.75: # if is a human and confidence at 75%
 
-                if detections.id[i].item() in list(tracker.keys()): # the detection is already known so stored in tracker
-                    oldx1, oldy1, oldx2, oldy2 = tracker[detections.id[i].item()] # get the stored coord to compare
-                    # we compare the difference with 15 so we can see the diff if the person is moving slowly 
-                    # (maybe the case for a blind person looking for a door)
-                    if ((x2-x1)-(oldx2-oldx1)) > 15 and ((y2-y1)-(oldy2-oldy1)) > 15: # the difference is positive and big enough so the person is getting closer
-                        print(f"{detections.id[i].item()} s'approche")
-                    elif ((x2-x1)-(oldx2-oldx1)) < -15 and ((y2-y1)-(oldy2-oldy1)) < -15: # the difference is negative and big enough so the person is stepping back
-                        print(f"{detections.id[i].item()} se recule")
-                tracker[detections.id[i].item()] = x1,y1,x2,y2 # store the new coord
+                    if detections.id[i].item() in list(tracker.keys()): # the detection is already known so stored in tracker
+                        oldx1, oldy1, oldx2, oldy2 = tracker[detections.id[i].item()] # get the stored coord to compare
+                        # we compare the difference with 15 so we can see the diff if the person is moving slowly 
+                        # (maybe the case for a blind person looking for a door)
+                        if ((x2-x1)-(oldx2-oldx1)) > 15 and ((y2-y1)-(oldy2-oldy1)) > 15: # the difference is positive and big enough so the person is getting closer
+                            print(f"{detections.id[i].item()} s'approche")
+                        elif ((x2-x1)-(oldx2-oldx1)) < -15 and ((y2-y1)-(oldy2-oldy1)) < -15: # the difference is negative and big enough so the person is stepping back
+                            print(f"{detections.id[i].item()} se recule")
+                    tracker[detections.id[i].item()] = x1,y1,x2,y2 # store the new coord
             
         # Display the annotated frame
         cv2.imshow("YOLOv8 Tracking", annotated_frame)
